7cbc3920899922ca2f35c4087ed4ebb7890e435d
Daniel Luo <d.luo117@gmail.com>
2021-08-23 12:10:33 -0700

    Create Xcode project with tentative working name

---
+++ Elbow Saver/Elbow_SaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Elbow_SaverApp.swift
+//  Elbow Saver
+//
+//  Created by Daniel Luo on 8/23/21.
+//
+
+import SwiftUI
+
+@main
+struct Elbow_SaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
